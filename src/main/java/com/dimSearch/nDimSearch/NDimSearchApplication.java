323b799add955916a640bbe07a2ff0248565402d
Patrick Woodworth <p77wood@gmail.com>
2024-07-31 09:22:08 -0500

    initial commit

---
+++ src/main/java/com/dimSearch/nDimSearch/NDimSearchApplication.java
@@ -0,0 +1,13 @@
+package com.dimSearch.nDimSearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NDimSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NDimSearchApplication.class, args);
+	}
+
+}
