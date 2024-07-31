e84127b072081e68daaf2e5e932a169ec6e40871
Patrick Woodworth <p77wood@gmail.com>
2024-07-31 09:25:34 -0500

    add controller

---
+++ src/main/java/com/dimSearch/nDimSearch/controller/SearchController.java
@@ -0,0 +1,17 @@
+package com.dimSearch.nDimSearch.controller;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.gridGenerator.Data.Cell;
+import org.gridGenerator.Service.WorldGenerationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+class SearchController {
+    private static final Logger log = LogManager.getLogger(SearchController.class.getName());
+    
+}
