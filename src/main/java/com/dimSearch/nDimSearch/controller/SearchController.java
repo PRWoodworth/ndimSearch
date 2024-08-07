66598cc8dc403ac3994ac9ef2de140215a2a4f0f
Patrick Woodworth <p77wood@gmail.com>
2024-08-06 22:18:13 -0500

    Added passing from controller.

--- src/main/java/com/dimSearch/nDimSearch/controller/SearchController.java
+++ src/main/java/com/dimSearch/nDimSearch/controller/SearchController.java
@@ -1,25 +1,24 @@
 package com.dimSearch.nDimSearch.controller;
 
+import com.dimSearch.nDimSearch.data.POJO.InputData;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.dimSearch.nDimSearch.service.NDimSearchService;
 
-import java.util.ArrayList;
 
 @RestController
 class SearchController {
     private static final Logger log = LogManager.getLogger(SearchController.class.getName());
 
-    @Autowired
-    NDimSearchService nDimSearchService;
+    @Autowired NDimSearchService searchService;
 
     @GetMapping("/search")
-    public void searchEndpoint(){
-
+    public void searchEndpoint(@RequestBody InputData requestBody){
+        searchService.search(requestBody.getSearchTarget(), requestBody.getInput());
     }
 
     
---
+++ src/main/java/com/dimSearch/nDimSearch/data/POJO/InputData.java
@@ -0,0 +1,16 @@
+package com.dimSearch.nDimSearch.data.POJO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class InputData {
+    private String searchTarget;
+    private ArrayList<Collection<?>> input;
+}
