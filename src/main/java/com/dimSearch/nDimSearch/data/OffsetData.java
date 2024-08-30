e5fae62f2310eb0e82a29200aefdfb35355d49e4
Patrick Woodworth <p77wood@gmail.com>
2024-08-30 13:23:41 -0500

    push() method equivalents not returning the updated object is so ungodly stupid

--- src/main/java/com/dimSearch/nDimSearch/data/OffsetData.java
+++ src/main/java/com/dimSearch/nDimSearch/data/OffsetData.java
@@ -3,9 +3,12 @@ package com.dimSearch.nDimSearch.data;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.Deque;
+
 @Data
 @AllArgsConstructor
-public class SplitOffsetHolder {
+public class OffsetData {
     private int upperSplitOffset;
     private int lowerSplitOffset;
+    private Deque offsetDirections;
 }
--- src/main/java/com/dimSearch/nDimSearch/service/NDimSearchService.java
+++ src/main/java/com/dimSearch/nDimSearch/service/NDimSearchService.java
@@ -2,14 +2,14 @@ package com.dimSearch.nDimSearch.service;
 
 import com.dimSearch.nDimSearch.data.DataHolder;
 import com.dimSearch.nDimSearch.data.SplitInputHolder;
-import com.dimSearch.nDimSearch.data.SplitOffsetHolder;
+import com.dimSearch.nDimSearch.data.OffsetData;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.List;
+import java.util.*;
 
 
 @Service
@@ -30,8 +30,9 @@ public class NDimSearchService {
     //TODO: return list of all locations where the target was found?
     public ResponseEntity<String> searchOperation(String searchTarget, List<DataHolder> input){
         ResponseEntity<String> searchResponseEntity;
+        Deque<Character> offsetDeque = new ArrayDeque<>();
         try{
-            SplitOffsetHolder offsetHolder = search(searchTarget, input, 0, 0);
+            OffsetData offsetHolder = search(searchTarget, input, offsetDeque, 0, 0);
             int originalIndex = splitOffsetBacktrack(offsetHolder, input.size());
             searchResponseEntity = new ResponseEntity<>("Target found at index ".concat(String.valueOf(originalIndex)), HttpStatus.OK);
         } catch (Exception e){
@@ -40,26 +41,41 @@ public class NDimSearchService {
         return searchResponseEntity;
     }
 
-    public SplitOffsetHolder search(String searchTarget, List<DataHolder> input, int upperSplitOffset, int lowerSplitOffset) {
+    public OffsetData search(String searchTarget, List<DataHolder> input, Deque<Character> offsetQueue, int upperOffset, int lowerOffset) {
         log.info("Input: {}", input.toString());
-        log.info("Upper split offset: {}", upperSplitOffset);
-        log.info("Lower split offset: {}", lowerSplitOffset);
+        log.info("Upper split offset: {}", upperOffset);
+        log.info("Lower split offset: {}", lowerOffset);
 
         if (input.size() > 1) {
             SplitInputHolder splitInput = split(input);
             log.info("Split Input: {}", splitInput.toString());
-            search(searchTarget, splitInput.getUpperHalf(), upperSplitOffset + 1, lowerSplitOffset);
-            search(searchTarget, splitInput.getLowerHalf(),  upperSplitOffset, lowerSplitOffset + 1);
+            search(searchTarget, splitInput.getUpperHalf(), offsetQueue.addFirst('U'), upperOffset + 1, lowerOffset);
+            search(searchTarget, splitInput.getLowerHalf(), offsetQueue.addFirst('L'), upperOffset, lowerOffset + 1);
         } else {
             if (input.get(0).getName().equalsIgnoreCase(searchTarget)) {
-                return new SplitOffsetHolder(upperSplitOffset, lowerSplitOffset);
+                return new OffsetData(upperOffset, lowerOffset, offsetQueue);
             }
         }
         return null;
     }
 
-    public int splitOffsetBacktrack(SplitOffsetHolder offsetHolder, int originalSize){
-//      TODO: calculate offset from original split based on number of upper/lower splits
+    public int splitOffsetBacktrack(OffsetData offsetHolder, int originalSize){
+//      TODO: calculate offset from original split based on number of upper/lower splits and original size
+        /*
+        determine how far to move from center
+        move direction up/down # of times equal to offset counts
+         */
+        int totalOffsets = offsetHolder.getLowerSplitOffset() + offsetHolder.getUpperSplitOffset();
+        int sizeHolder = originalSize;
+        int index = sizeHolder/2;
+        int offsetMove = 0;
+        Deque offsetDeque = offsetHolder.getOffsetDirections();
+        char offsetDirection;
+        for (int i = 0; i < totalOffsets; i++) {
+            offsetMove = sizeHolder/2;
+            offsetDirection = offsetHolder.getOffsetDirections().pop().toString().charAt(0);
+            sizeHolder /= 2;
+        }
         return 0;
     }
 }
