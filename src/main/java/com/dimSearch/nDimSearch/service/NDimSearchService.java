c645d09f71dba5debc485fd072acacea74031009
Woodworth <patrick.woodworth@capgemini.com>
2024-09-11 14:12:28 -0500

    Search works, returns list of indices where search target was found.

--- src/main/java/com/dimSearch/nDimSearch/service/NDimSearchService.java
+++ src/main/java/com/dimSearch/nDimSearch/service/NDimSearchService.java
@@ -15,55 +15,34 @@ import java.util.*;
 public class NDimSearchService {
     private static final Logger log = LogManager.getLogger(NDimSearchService.class.getName());
 
-//        TODO: implement basic recursive half/half search first on unsorted 1d array
 //        TODO: expand to 2d search
 //        TODO: create thread for each recursive (one for upper half, one for lower half)?
 //        TODO: generalize to n-d
-
-//    TODO: recursively flatten each level until single flat array?
 
     public SplitInputHolder split(List<DataHolder> input){
         return new SplitInputHolder(input.subList(0, input.size()/2), input.subList((input.size()/2), input.size()));
     }
 
-    public Deque<Character> updateAndReturn(Deque<Character> offsetQueue, char charToAdd){
-        offsetQueue.addFirst(charToAdd);
-        return offsetQueue;
-    }
-
-    //TODO: return list of all locations where the target was found?
     public ResponseEntity<String> searchOperation(String searchTarget, List<DataHolder> input){
-        ResponseEntity<String> searchResponseEntity = new ResponseEntity<>("REQUEST_TEMPLATE", HttpStatus.ACCEPTED);
         try{
-            int originalSize = input.size();
-            log.info("Input size: {}", originalSize);
-            int originalIndex = search(searchTarget, input, input.size()/2);
-            switch(originalIndex){
-                case -1: return new ResponseEntity<>("Target not found within given input ", HttpStatus.OK);
-                default: return new ResponseEntity<>("Target found at index ".concat(String.valueOf(originalIndex)), HttpStatus.OK);
-            }
+            ArrayList<Integer> positiveSearchReturns = search(searchTarget, input, input.size()/2, new ArrayList<>());
+            return new ResponseEntity<>(positiveSearchReturns.toString(), HttpStatus.OK);
         } catch (Exception e){
             return new ResponseEntity<>("Error occurred during search.", HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
-    public int search(String searchTarget, List<DataHolder> input, int midIndex) {
-        log.info("Input: {}", input.toString());
-        log.info("Mid Index: {}", String.valueOf(midIndex));
-
+    public ArrayList<Integer> search(String searchTarget, List<DataHolder> input, int midIndex, ArrayList<Integer> positiveReturns) {
         if (input.size() > 1) {
             SplitInputHolder splitInput = split(input);
-            log.info("Split Input: {}", splitInput.toString());
-            search(searchTarget, splitInput.getUpperHalf(), (int) (midIndex + Math.ceil(splitInput.getUpperHalf().size()) / 2));
-            search(searchTarget, splitInput.getLowerHalf(), (int) (midIndex - Math.floor(splitInput.getLowerHalf().size()) / 2));
+            search(searchTarget, splitInput.getUpperHalf(), (int) (midIndex + Math.ceil(splitInput.getUpperHalf().size()) / 2), positiveReturns);
+            search(searchTarget, splitInput.getLowerHalf(), (int) (midIndex - Math.floor(splitInput.getLowerHalf().size()) / 2), positiveReturns);
         } else {
             if (input.get(0).getName().equalsIgnoreCase(searchTarget)) {
-                log.info("Search target found at {}", midIndex);
-                return midIndex;
-                //TODO: finds value correctly but later returns override the value
+                positiveReturns.add(midIndex);
             }
         }
-        return -1;
+        return positiveReturns;
     }
 }
 
