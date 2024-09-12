9fbba8d65a0f1cc34ec2bb695d904c03466b9aa5
Woodworth <patrick.woodworth@capgemini.com>
2024-09-12 11:15:19 -0500

    added file for future

---
+++ src/main/java/com/dimSearch/nDimSearch/service/NDimSearchDataGenerator.java
@@ -0,0 +1,8 @@
+package com.dimSearch.nDimSearch.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class NDimSearchDataGenerator {
+//    TODO: generate an absolutely massive dataset here to search for something in, for testing purposes.
+}
